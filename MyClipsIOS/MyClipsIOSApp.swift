f276a92f83b93a6e57a49c25da5461f35d9aa6d0
polmoya <moyapol@gmail.com>
2024-09-04 16:30:20 +0200

    Initial Commit

---
+++ MyClipsIOS/MyClipsIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyClipsIOSApp.swift
+//  MyClipsIOS
+//
+//  Created by Pol Moya Betriu on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MyClipsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
